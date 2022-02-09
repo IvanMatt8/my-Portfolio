d0556e5cb6136ba1d8a49ad58101592bc90e21fa
IvanMatt8 <ivanmatt1995@gmail.com>
2022-02-09 12:14:09 -0300

    :sparkles: component footer com style

---
+++ src/components/Footer/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./index.css";
+
+
+
+function Footer(){
+    return(
+        <footer>
+            <p> © Criado e desenvolvido por Ivan Matheus</p>
+        </footer>
+    )
+}
+
+
+
+export default Footer
